e818182df246e09b5a20fa7f7354cd46688c3928
Moharram Ansari <moharramansari@gmail.com>
2023-02-28 12:42:30 +0530

    Added blog schema module

---
+++ models/blogModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+//Declare the schema for the mongo model
+
+let blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    numViews: {
+      type: Number,
+      required: true,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    image: {
+      type: String,
+      default: "link of image",
+    },
+    author: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+//export the model
+module.exports = mongoose.model("Blog", blogSchema);
