199d6f42aedbf928401462982390dec2ff82e3a6
Moharram Ansari <moharramansari@gmail.com>
2023-03-02 19:14:02 +0530

    Make changes in middleware uploadImages module

--- middleware/uploadImages.js
+++ middleware/uploadImages.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 const sharp = require("sharp");
 const path = require("path");
+const fs = require("fs");
 
 const multerStorage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -39,7 +40,8 @@ const productImgResize = async (req, res, next) => {
         .resize(300, 300)
         .toFormat("jpeg")
         .jpeg({ quality: 90 })
-        .toFile(`public/images/product/${file.filename}`);
+        .toFile(`public/images/products/${file.filename}`);
+      fs.unlinkSync(`public/images/products/${file.filename}`);
     })
   );
   next();
@@ -54,6 +56,7 @@ const blogImgResize = async (req, res, next) => {
         .toFormat("jpeg")
         .jpeg({ quality: 90 })
         .toFile(`public/images/blogs/${file.filename}`);
+      fs.unlinkSync(`public/images/blogs/${file.filename}`);
     })
   );
   next();
